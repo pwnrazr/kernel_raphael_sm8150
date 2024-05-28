da65f74a3eeffb8a9d0cf897be0360506dfa58d5
John Galt <johngaltfirstrun@gmail.com>
2024-05-28 09:24:36 -0400

    Revert "BACKPORT: cpuidle: teo: Optionally skip polling states in teo_find_shallower_state()"
    This reverts commit f49d20369e67d7ab95ed381274e0a02b8abc4fd8.
    
    Part of util awareness change (which we did not backport), reference:
    https://github.com/kerneltoast/android_kernel_google_zuma/commit/bd5e386af1692018fe0360b297caee5f225ad2e2

--- drivers/cpuidle/governors/teo.c
+++ drivers/cpuidle/governors/teo.c
@@ -284,17 +284,15 @@ static bool teo_state_ok(int i, struct cpuidle_driver *drv)
  * @dev: Target CPU.
  * @state_idx: Index of the capping idle state.
  * @duration_us: Idle duration value to match.
- * @no_poll: Don't consider polling states.
  */
 static int teo_find_shallower_state(struct cpuidle_driver *drv,
 				    struct cpuidle_device *dev, int state_idx,
-				    s64 duration_us, bool no_poll)
+				    s64 duration_us)
 {
 	int i;
 
 	for (i = state_idx - 1; i >= 0; i--) {
-		if (dev->states_usage[i].disable ||
-				(no_poll && drv->states[i].flags & CPUIDLE_FLAG_POLLING))
+		if (dev->states_usage[i].disable)
 			continue;
 
 		state_idx = i;
@@ -497,7 +495,7 @@ static int teo_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 	 * candidate state, a shallower one needs to be found.
 	 */
 	if (drv->states[idx].target_residency > duration_us) {
-		i = teo_find_shallower_state(drv, dev, idx, duration_us, false);
+		i = teo_find_shallower_state(drv, dev, idx, duration_us);
 		if (teo_state_ok(i, drv))
 			idx = i;
 	}
