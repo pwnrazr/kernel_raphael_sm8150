5330fdfd7541e5f94334f535c9f242d9c9b75bbe
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2024-06-24 23:22:18 +0800

    Revert "Revert "sbalance: balance on fork""
    This reverts commit 3b71010276f07828e19fa8b51a5b91a8d17bb885.

--- kernel/fork.c
+++ kernel/fork.c
@@ -98,6 +98,7 @@
 
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/irq.h>
 #include <linux/event_tracking.h>
 
 #include <asm/pgtable.h>
@@ -2266,6 +2267,7 @@ long _do_fork(unsigned long clone_flags,
 			cpu_input_boost_kick_max(500, false);
 			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500, true);
 			devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500, true);
+			balance_irqs();
 		}
 	}
 
